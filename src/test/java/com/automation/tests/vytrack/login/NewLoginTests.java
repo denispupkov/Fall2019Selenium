e8f5f4b316426d52162bdbf829c4af092ca93553
Denis Pupkov <d3664s@outlook.com>
2020-04-05 10:57:00 -0400

    Selenium day 15

--- src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
@@ -2,25 +2,54 @@ package com.automation.tests.vytrack.login;
 
 import com.automation.pages.LoginPage;
 import com.automation.tests.vytrack.AbstractTestBase;
+import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.Driver;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class NewLoginTests extends AbstractTestBase {
 
 
-    @Test
+    @Test(groups = "smoke")
     public void verifyPageTitle(){
+        test = report.createTest("Verify page title");
         LoginPage loginPage = new LoginPage();
         loginPage.login();
         Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard");
+        test.pass("Page title Dashboard was verified");
     }
 
     @Test
     public void verifyWarningMessage () {
+        test = report.createTest("Verify warning message");
         LoginPage loginPage = new LoginPage();
         loginPage.login("wrong","wrong");
         Assert.assertEquals(loginPage.getWarningMessageText(),"Invalid user name or password.");
+        //take a screenshot
+        BrowserUtils.getScreenshot("warning_message");
+        test.pass("Warning message is displayed");
 }
+
+    @Test(dataProvider = "credentials")
+    public void loginWithDDT(String userName, String password) {
+        test = report.createTest("Verify page title");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(userName, password);
+        test.info("Login as " + userName);//log some steps
+        BrowserUtils.wait(3);Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard");
+        test.pass("Page title Dashboard was verified");
+    }
+    //Object[][] or Object[] or Iterator<Object[]>
+//Object[] - 1 column with a data
+//Object[][] 2+
+    @DataProvider
+    public Object[][] credentials() {
+        return new Object[][]{
+                {"storemanager85", "UserUser123"},
+                {"salesmanager110", "UserUser123"},
+                {"user16", "UserUser123"},
+        };
+    }
 
 }
